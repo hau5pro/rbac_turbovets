fc8dec712c36896935ea47acc113c84fbd70b067
House Yokeswaran <h4u5d3v@gmail.com>
2025-05-02 11:52:31 -0400

    fix perm decorator

--- src/infrastructure/decorators/permissions.decorator.ts
+++ src/infrastructure/decorators/permissions.decorator.ts
@@ -2,6 +2,7 @@ import { Permission } from '../enums';
 import { SetMetadata } from '@nestjs/common';
 
 export const PERMISSIONS = 'permissions';
+
 export const Permissions = (...permissions: Permission[]) => {
-  SetMetadata(PERMISSIONS, permissions);
+  return SetMetadata(PERMISSIONS, permissions);
 };
